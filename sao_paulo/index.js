9305c2ee4c60b3aed07ee850091ec2458e7d268b
Dyego Eugenio <deugenio@daitangroup.com>
2018-04-16 16:01:59 -0300

    add comment

--- sao_paulo/index.js
+++ sao_paulo/index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Sao_Paulo");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Sao Paulo is: ' + currentTime.toString(),
+        body: '#### The time in Sao Paulo is: ' + currentTime.toString(),
     });
 };
