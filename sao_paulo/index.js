09f2e726898dcc36f0ef546ebc93b53b46e829c2
Dyego Eugenio <deugenio@daitangroup.com>
2018-04-16 15:13:09 -0300

    testing with sub dirs

---
+++ sao_paulo/index.js
@@ -0,0 +1,9 @@
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date(); 
+    currentTime.setTimezone("America/Sao_Paulo");
+    callback(null, {
+        statusCode: '200',
+        body: 'The time in Sao Paulo is: ' + currentTime.toString(),
+    });
+};
